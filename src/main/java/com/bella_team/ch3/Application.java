ad60bace40d167b2714961b8750eb419a39d272e
Alexey Shamrikov <92291588+lebzdel@users.noreply.github.com>
2022-07-17 00:02:44 +0300

    Initial commit

---
+++ src/main/java/com/bella_team/ch3/Application.java
@@ -0,0 +1,13 @@
+package com.bella_team.ch3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bella_team/ch3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bella_team.ch3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
